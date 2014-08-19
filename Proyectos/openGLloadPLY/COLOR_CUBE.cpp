4c1ee171dd7e8aa42602f66f7e443f1e64e92516
Gonzalo Martinez <gonzalo.martinez91@gmail.com>
2014-08-19 19:10:39 -0300

    Proyeccion sobre hromiga

--- Proyectos/openGLloadPLY/COLOR_CUBE.cpp
+++ Proyectos/openGLloadPLY/COLOR_CUBE.cpp
@@ -4,9 +4,13 @@
 #include "Model_PLY.h"
 #include<iostream>
 
+#include "FreeImage.h"
+#include "matrix4x4.h"
+
 using namespace std;
 
 Model_PLY* model = NULL;
+GLuint texObject, projTexture;
 GLfloat vertices[][3]={{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},
           {1.0,1.0,-1.0},{-1.0,1.0,-1.0},{-1.0,-1.0,1.0},
            {1.0,-1.0,1.0},{1.0,1.0,1.0},{-1.0,1.0,1.0}};
@@ -111,6 +115,90 @@ void myReshape(int w, int h)
 	glMatrixMode(GL_MODELVIEW);
 }
 
+void BindProjectiveTexture(bool exp)
+{
+	if(exp)
+		glBindTexture(GL_TEXTURE_2D, projTexture);
+	else
+		glBindTexture(GL_TEXTURE_2D, texObject);
+}
+
+void loadLightMapTexture(const char *name)
+{
+	GLfloat eyePlaneS[] =  {1.0f, 0.0f, 0.0f, 0.0f};
+	GLfloat eyePlaneT[] =  {0.0f, 1.0f, 0.0f, 0.0f};
+	GLfloat eyePlaneR[] =  {0.0f, 0.0f, 1.0f, 0.0f};
+	GLfloat eyePlaneQ[] =  {0.0f, 0.0f, 0.0f, 1.0f};
+    GLfloat borderColor[4] = {0.6f, 0.6f, 0.6f, 1.0f};
+
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
+	glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE);
+
+	//set up texture generation mode and set the corresponding planes
+	glTexGeni(GL_S,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+	glTexGenfv(GL_S,GL_EYE_PLANE,eyePlaneS);
+	glTexGeni(GL_T,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+	glTexGenfv(GL_T,GL_EYE_PLANE,eyePlaneT);
+	glTexGeni(GL_R,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+	glTexGenfv(GL_R,GL_EYE_PLANE,eyePlaneR);
+	glTexGeni(GL_Q,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+	glTexGenfv(GL_Q,GL_EYE_PLANE,eyePlaneQ);
+
+	glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_GEN_T);
+    glEnable(GL_TEXTURE_GEN_R);
+    glEnable(GL_TEXTURE_GEN_Q);
+
+    FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+    FIBITMAP *dib(0);
+    BYTE* bits(0);
+    unsigned int width(0), height(0);
+    GLuint gl_texID;
+
+    fif = FreeImage_GetFileType(name, 0);
+    if(fif == FIF_UNKNOWN)
+        fif = FreeImage_GetFIFFromFilename(name);
+    if(fif == FIF_UNKNOWN)
+        printf("Formato de imagen no reconocido.");
+
+    if(FreeImage_FIFSupportsReading(fif))
+        dib = FreeImage_Load(fif, name);
+    if(!dib)
+        printf("Error al cargar la imagen.");
+
+    bits = FreeImage_GetBits(dib);
+    width = FreeImage_GetWidth(dib);
+    height = FreeImage_GetHeight(dib);
+    if((bits == 0) || (width == 0) || (height == 0))
+        printf("Error en la imagen.");
+
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, 0x812D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, 0x812D);
+	glTexParameterfv(GL_TEXTURE_2D,GL_TEXTURE_BORDER_COLOR,borderColor);
+    gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGB,width,height,GL_RGB,GL_UNSIGNED_BYTE,bits);
+
+	FreeImage_Unload(dib);
+}
+
+void textureProjection(Matrix4x4f &mv)
+{
+	Matrix4x4f inverseMV = Matrix4x4f::invertMatrix(mv);
+
+	//here is where we do the transformations on the texture matrix for the lightmap
+	//the basic equation here is M = Bias * Scale * ModelView for light map * Inverse Modelview
+	glMatrixMode(GL_TEXTURE);
+	glLoadIdentity();
+	glTranslatef(0.5f,0.5f,0.0f); //Bias
+	glScalef(0.5f,0.5f,1.0f);		//Scale
+	glFrustum(-0.035,0.035,-0.035,0.035,0.1,1.9); //MV for light map
+	glTranslatef(0.0f,0.0f,-1.0f);
+	glMultMatrixf(inverseMV.getMatrix());	//Inverse ModelView
+	glMatrixMode(GL_MODELVIEW);
+}
+
 int main(int argc, char **argv)
 {
     model = new Model_PLY();
@@ -130,7 +218,13 @@ int main(int argc, char **argv)
 	glutMouseFunc(mouse);
 	glutKeyboardFunc(keys);
 	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_TEXTURE_2D);
+
+	loadLightMapTexture("lightmap.bmp");
+
 	glutMainLoop();
 }
+
 
 
