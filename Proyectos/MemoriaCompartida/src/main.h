2b0833e41594460517b9cf0dc5638a9e68742be7
Gonzalo Martinez <gonzalo.martinez91@gmail.com>
2014-09-21 16:55:10 -0300

    Memoria compartida en dll

---
+++ Proyectos/MemoriaCompartida/src/main.h
@@ -0,0 +1,41 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <windows.h>
+#include "ofxSharedMemory.h"
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
+
+struct FaceStruct
+{
+    float p1[3];
+    float p2[3];
+    float p3[3];
+};
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+void DLL_EXPORT SomeFunction(const LPCSTR sometext);
+
+void DLL_EXPORT compartirMemoria(int numberFaces, FaceStruct* faces);
+
+void DLL_EXPORT leerMemoria(int* numberFaces, FaceStruct** faces);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__
