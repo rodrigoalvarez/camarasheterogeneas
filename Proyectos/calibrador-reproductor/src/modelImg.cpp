eb4797dc49baf593744de0414ea53514397ca94e
ralvarez <ralvarez@sophilabs.com>
2014-09-27 20:28:02 -0300

    Update calibrator and player project...

--- Proyectos/calibrador-reproductor/src/modelImg.cpp
+++ Proyectos/calibrador-reproductor/src/modelImg.cpp
@@ -1,5 +1,4 @@
 #include "modelImg.h"
-#include "FreeImage.h"
 #include <vector>
 #include <limits>
 #include <cmath>
@@ -41,31 +40,59 @@ void Model_IMG::MemoryLoad() {
         Id = *id;
         Width = *wPixels;
         Height = *hPixels;
-        Pixels = new char[Width * Height * 3];
+        Pixels = new unsigned char[Width * Height * 3];
         memcpy(Pixels, pixels, sizeof(char) * Width * Height * 3);
     }
 }
 
+int sizeToPowerTwoLessInt(int n) {
+    int res = 1;
+    while (res < n) {
+        res *= 2;
+    }
+    return res / 2;
+}
+int sizeToPowerTwoMoreInt(int n) {
+    int res = 1;
+    while (res < n) {
+        res *= 2;
+    }
+    return res;
+}
+
+int sizeToPowerFourLessInt(int n) {
+    int res = n;
+    while (res % 4 != 0) {
+        res -= 2;
+    }
+    return res;
+}
+int sizeToPowerFourMoreInt(int n) {
+    int res = n;
+    while (res % 4 != 0) {
+        res += 2;
+    }
+    return res;
+}
+
 void Model_IMG::Load(string filename) {
 
-    FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+    ofImage auxImg;
+    auxImg.loadImage(filename);
+
+    ofBuffer imageBuffer;
+    ofSaveImage(auxImg.getPixelsRef(), imageBuffer, OF_IMAGE_FORMAT_JPEG);
+
+    FIMEMORY* stream = FreeImage_OpenMemory((unsigned char*) imageBuffer.getBinaryBuffer(), imageBuffer.size());
+
+    FREE_IMAGE_FORMAT fif   = FreeImage_GetFileTypeFromMemory( stream, 0 );
+
     FIBITMAP *dib(0);
-    GLuint gl_texID;
+    dib = FreeImage_LoadFromMemory(fif, stream);
 
-    fif = FreeImage_GetFileType(filename.c_str(), 0);
-    if(fif == FIF_UNKNOWN)
-        fif = FreeImage_GetFIFFromFilename(filename.c_str());
-    if(fif == FIF_UNKNOWN)
-        cout << "Formato de imagen no reconocido." << endl;
-    if(FreeImage_FIFSupportsReading(fif))
-        dib = FreeImage_Load(fif, filename.c_str());
-    if(!dib)
-        cout << "Error al cargar la imagen." << endl;
+    Pixels = (unsigned char*)FreeImage_GetBits(dib);
 
-    Pixels = (char*)FreeImage_GetBits(dib);
     Width = FreeImage_GetWidth(dib);
     Height = FreeImage_GetHeight(dib);
-    if((Pixels == 0) || (Width == 0) || (Height == 0))
-        cout << "Error en la imagen." << endl;
     Id++;
 }
