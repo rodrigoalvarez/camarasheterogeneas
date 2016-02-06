dc8d6bd4491250c4b40f9c9ba7aedb6f8bdc0c32
Rodrigo Alvarez <ralvarez@sophilabs.com>
2016-02-06 12:58:53 -0300

    Le hicimos cambios a la libreria de compartir memoria.

--- Projects/Dlls/SharedMemory/src/main.cpp
+++ Projects/Dlls/SharedMemory/src/main.cpp
@@ -1,236 +1,240 @@
 #include "main.h"
 
-ofxSharedMemory<int*> nFacesMemoryMappedFile;
-ofxSharedMemory<FaceStruct*> facesMemoryMappedFile;
-
-bool isConnected;
-
-string facesMemoryKey = "Faces";
-string nFacesMemoryKey = "NumberFaces";
-int nFacesMemorySize = sizeof(int);
-int facesMemorySize;
-
-void DLL_EXPORT ReadSharedImage(int* Id, int* wPixels, int* hPixels , unsigned char** pixels) {
-
-    ofxSharedMemory<int*> memoryMappedImageId;
-    ofxSharedMemory<int*> memoryMappedImageSizeW;
-    ofxSharedMemory<int*> memoryMappedImageSizeH;
-    ofxSharedMemory<unsigned char*> memoryMappedImage;
-
-    bool isConnectedImageId;
-    bool isConnectedPixels;
-    bool isConnectedWPixels;
-    bool isConnectedHPixels;
-
-    std::stringstream key1;
-    key1 << "ImageId" << *Id / 10000;
-    int* idMomentoNuevo;
-    memoryMappedImageId.setup(key1.str(), sizeof(int), false);
-    isConnectedImageId = memoryMappedImageId.connect();
-    if (isConnectedImageId) {
-        idMomentoNuevo = memoryMappedImageId.getData();
-        std::stringstream key2;
-        key2 << "ImagePixelsW" << *Id / 10000;
-        memoryMappedImageSizeW.setup(key2.str(), sizeof(int), false);
-        isConnectedWPixels = memoryMappedImageSizeW.connect();
-        if (isConnectedWPixels) {
-            *wPixels = *memoryMappedImageSizeW.getData();
-
-            std::stringstream key3;
-            key3 << "ImagePixelsH" << *Id / 10000;
-            memoryMappedImageSizeH.setup(key3.str(), sizeof(int), false);
-            isConnectedHPixels = memoryMappedImageSizeH.connect();
-            if (isConnectedHPixels) {
-                *hPixels = *memoryMappedImageSizeH.getData();
-                std::stringstream key4;
-                key4 << "ImagePixels" << *Id / 10000;
-                memoryMappedImage.setup(key4.str(), sizeof(unsigned char) * (*wPixels) * (*hPixels) * 3, false);
-                isConnectedPixels = memoryMappedImage.connect();
-                if (isConnectedPixels) {
-
-                    *pixels = memoryMappedImage.getData();
-                    *Id = *idMomentoNuevo;
-                }
-                else{
-                    *wPixels = 0;
-                    *hPixels = 0;
-                    *Id = -1;
-                }
-            }
-            else{
-                *hPixels = 0;
-                *Id = -1;
-            }
-        }
-        else{
-            *wPixels = 0;
-            *Id = -1;
-        }
-    }
-    else
-        *Id = -1;
-}
-
-void MemoryCheck(int* Id) {
-
-    ofxSharedMemory<int*> memoryMappedImageId;
-
-    bool isConnectedImageId;
-
-    std::stringstream key1;
-    key1 << "ImageId" << *Id / 10000;
-    int* idMomentoNuevo;
-
-    memoryMappedImageId.setup(key1.str(), sizeof(int), false);
-
-    isConnectedImageId = memoryMappedImageId.connect();
-
-    if (isConnectedImageId) {
-        idMomentoNuevo = memoryMappedImageId.getData();
-        if (*idMomentoNuevo > *Id)
-            *Id = *idMomentoNuevo;
-    }
-    else
-        *Id = -1;
-}
-
-void DLL_EXPORT ShareImage(int* imageId, unsigned char* pixels, int* wPixels, int* hPixels) {
-
-    ofxSharedMemory<int*> memoryMappedImageId;
-    ofxSharedMemory<int*> memoryMappedImageSizeW;
-    ofxSharedMemory<int*> memoryMappedImageSizeH;
-    ofxSharedMemory<unsigned char*> memoryMappedImage;
-    bool isConnectedImageId;
-    bool isConnectedPixels;
-    bool isConnectedWPixels;
-    bool isConnectedHPixels;
-
-    std::stringstream key1;
-    key1 << "ImageId" << *imageId / 10000;
-    std::stringstream key2;
-    key2 << "ImagePixelsW" << *imageId / 10000;
-    std::stringstream key3;
-    key3 << "ImagePixelsH" << *imageId / 10000;
-    std::stringstream key4;
-    key4 << "ImagePixels" << *imageId / 10000;
-
-    bool shareImage = false;
-    while (!shareImage){
-        memoryMappedImageId.setup(key1.str(), sizeof(int), true);
-        isConnectedImageId = memoryMappedImageId.connect();
-        if (isConnectedImageId){
-            memoryMappedImageId.setData(imageId);
-
-            memoryMappedImageSizeW.setup(key2.str(), sizeof(int), true);
-            isConnectedWPixels = memoryMappedImageSizeW.connect();
-            if (isConnectedWPixels){
-                memoryMappedImageSizeW.setData(wPixels);
-
-                memoryMappedImageSizeH.setup(key3.str(), sizeof(int), true);
-                isConnectedHPixels = memoryMappedImageSizeH.connect();
-                if (isConnectedHPixels){
-                    memoryMappedImageSizeH.setData(hPixels);
-
-                    memoryMappedImage.setup(key4.str(), sizeof(unsigned char) * (*wPixels) * (*hPixels) * 3, true);
-                    isConnectedPixels = memoryMappedImage.connect();
-                    if (isConnectedPixels){
-
-                        memoryMappedImage.setData(pixels);
-                        shareImage = true;
-                    }
-                }
-            }
-        }
-    }
-}
+ofxSharedMemory<int*> memoryMappedMeshId;
+ofxSharedMemory<int*> memoryMappedMeshSize;
+ofxSharedMemory<FaceStruct*> memoryMappedMesh;
 
 bool isConnectedMeshId;
-bool isConnectedFaces;
-bool isConnectedNFaces;
+bool isConnectedMeshSize;
+bool isConnectedMesh;
 
-ofxSharedMemory<int*> memoryMappedMeshId;
-ofxSharedMemory<FaceStruct*> memoryMappedMesh;
-ofxSharedMemory<int*> memoryMappedMeshSize;
+std::stringstream MeshIdKey;
+std::stringstream MeshSizeKey;
+std::stringstream MeshKey;
 
 
-void DLL_EXPORT ShareMesh(int idMesh, int numberFaces, FaceStruct* faces) {
+//bool firstTimeMesh = true;
 
-    std::stringstream key1;
-    key1 << "MeshId" << idMesh / 10000;
-	memoryMappedMeshId.setup(key1.str(), sizeof(int), true);
-    isConnectedMeshId = memoryMappedMeshId.connect();
-    if (isConnectedMeshId){
-        memoryMappedMeshId.setData(&idMesh);
+MemoryMeshStruct* meshMemory = new MemoryMeshStruct[100];
+int meshIndexMemory = 0;
 
-        std::stringstream key2;
-        key2 << "MeshNumberFaces" << idMesh / 10000;
-        memoryMappedMeshSize.setup(key2.str(), sizeof(int), true);
-        isConnectedNFaces = memoryMappedMeshSize.connect();
-        if (isConnectedNFaces){
-            int* nfaces = new int;
-            *nfaces = numberFaces;
-            memoryMappedMeshSize.setData(nfaces);
+void DLL_EXPORT ShareMeshSetup(int* meshId, int* index) {
 
-            std::stringstream key3;
-            key3 << "MeshFaces" << idMesh;
-            isConnectedFaces = false;
-            while (!isConnectedFaces){
-                memoryMappedMesh.setup(key3.str(), sizeof(FaceStruct) * numberFaces, true);
-                isConnectedFaces = memoryMappedMesh.connect();
-                if (isConnectedFaces){
-                    memoryMappedMesh.setData(faces);
-                }
-            }
-        }
-    }
+    *index = meshIndexMemory;
 
-    isConnectedMeshId = false;
-    isConnectedNFaces = false;
-    isConnectedFaces = false;
+    meshIndexMemory++;
+
+    meshMemory[*index].maxNumberFaces = 0;
+
+    meshMemory[*index].firstTimeMesh = true;
+
+    meshMemory[*index].MeshIdKey << "MeshId" << *meshId / 10000;
+
+    meshMemory[*index].MeshSizeKey << "MeshSize" << *meshId / 10000;
+
+    meshMemory[*index].MeshKey << "Mesh" << *meshId / 10000;
+
+	meshMemory[*index].memoryMappedMeshId.setup(meshMemory[*index].MeshIdKey.str(), sizeof(int), true);
+    isConnectedMeshId = meshMemory[*index].memoryMappedMeshId.connect();
+
+	meshMemory[*index].memoryMappedMeshSize.setup(meshMemory[*index].MeshSizeKey.str(), sizeof(int), true);
+    isConnectedMeshSize = meshMemory[*index].memoryMappedMeshSize.connect();
+
+    //memoryMappedMesh.setup(MeshKey.str(), sizeof(FaceStruct) * maxNumberFaces, true);
+    //isConnectedMesh = memoryMappedMesh.connect();
 
 }
 
-void DLL_EXPORT ReadSharedMesh(int* Id, int* numberFaces, FaceStruct** faces)
-{
-    std::stringstream key1;
-    key1 << "MeshId" << *Id / 10000;
-    int* id = NULL;
+void DLL_EXPORT ReadSharedMeshSetup(int* meshId, int* index) {
 
-    memoryMappedMeshId.setup(key1.str(), sizeof(int), false);
-    isConnectedMeshId = memoryMappedMeshId.connect();
-    if (isConnectedMeshId) {
+    meshIndexMemory++;
 
-        id = memoryMappedMeshId.getData();
+    *index = meshIndexMemory;
 
-        std::stringstream key2;
-        key2 << "MeshNumberFaces" << *Id / 10000;
+    meshMemory[*index].maxNumberFaces = 0;
 
-        memoryMappedMeshSize.setup(key2.str(), sizeof(int), false);
-        isConnectedNFaces = memoryMappedMeshSize.connect();
-        if (isConnectedNFaces) {
-            *numberFaces = *memoryMappedMeshSize.getData();
-            std::stringstream key3;
-            key3 << "MeshFaces" << *id;
-            memoryMappedMesh.setup(key3.str(), sizeof(FaceStruct) * (*numberFaces), false);
-            isConnectedFaces = memoryMappedMesh.connect();
+    meshMemory[*index].firstTimeMesh = true;
 
-            if (isConnectedFaces) {
-                *faces = memoryMappedMesh.getData();
-            }
-            else
-                *numberFaces = 0;
-        }
-    }
+    meshMemory[*index].MeshIdKey << "MeshId" << *meshId / 10000;
 
-    if ((id != NULL && *id > *Id) && (numberFaces != NULL && *numberFaces > 0)) {
-        *Id = *id;
-    }
-    else{
-        *Id = -1;
-    }
+    meshMemory[*index].MeshSizeKey << "MeshSize" << *meshId / 10000;
+
+    meshMemory[*index].MeshKey << "Mesh" << *meshId / 10000;
+
+	meshMemory[*index].memoryMappedMeshId.setup(meshMemory[*index].MeshIdKey.str(), sizeof(int), false);
+    isConnectedMeshId = meshMemory[*index].memoryMappedMeshId.connect();
+
+	meshMemory[*index].memoryMappedMeshSize.setup(meshMemory[*index].MeshSizeKey.str(), sizeof(int), false);
+    isConnectedMeshSize = meshMemory[*index].memoryMappedMeshSize.connect();
 
 }
 
+void DLL_EXPORT ShareMesh( int* meshId, int* numberFaces, FaceStruct* faces, int* index) {
+
+    meshMemory[*index].memoryMappedMeshId.setData(meshId);
+
+    /*if (*maxNumberFaces < *numberFaces){
+        cout << (*numberFaces * 10) << endl;
+        *maxNumberFaces = *numberFaces * 10;
+        memoryMappedMesh.setup(MeshKey.str(), sizeof(FaceStruct) * 2 ***** (*maxNumberFaces), true);
+        isConnectedMesh = memoryMappedMesh.connect();
+    }*/
+    //meshMemory[*index].maxNumberFaces = numberFaces;
+    meshMemory[*index].memoryMappedMesh.setup(meshMemory[*index].MeshKey.str(), sizeof(FaceStruct) * (*numberFaces), true);
+    isConnectedMesh = meshMemory[*index].memoryMappedMesh.connect();
+
+    cout << "x1" << endl;
+    if (*numberFaces > 1) {
+        cout << faces[0].p1[0] << " - " << faces[0].p1[1] << " - " << faces[0].p1[2] << " - ";
+        cout << faces[1].p1[0] << " - " << faces[1].p1[1] << " - " << faces[1].p1[2] << endl;
+
+    } else {
+        cout << faces[0].p1[0] << " - " << faces[0].p1[1] << " - " << faces[0].p1[2] << endl;
+
+    }
+    cout << "x2" << endl;
+
+    meshMemory[*index].memoryMappedMesh.setData(faces);
+
+    //int * pepe = new int;
+    //*pepe = 2;
+    //memoryMappedMeshSize.setData(pepe/*maxNumberFaces*/);
+    meshMemory[*index].memoryMappedMeshSize.setData(numberFaces);
+
+}
+
+void DLL_EXPORT ReadSharedMesh(int* meshId, int* numberFaces, FaceStruct** faces, int* index) {
+
+    *meshId = *(meshMemory[*index].memoryMappedMeshId).getData();
+
+    *numberFaces = *(meshMemory[*index].memoryMappedMeshSize).getData();
+
+    if (meshMemory[*index].maxNumberFaces != *numberFaces){//meshMemory[*index].maxNumberFaces < *numberFaces
+        meshMemory[*index].maxNumberFaces = *numberFaces;
+        //meshMemory[*index].maxNumberFaces = *numberFaces * 10;
+        meshMemory[*index].memoryMappedMesh.setup(meshMemory[*index].MeshKey.str(), sizeof(FaceStruct) * (*numberFaces), false);
+        isConnectedMesh = meshMemory[*index].memoryMappedMesh.connect();
+    }
+
+    *faces = meshMemory[*index].memoryMappedMesh.getData();
+
+    cout << "x1" << endl;
+    //cout << faces << endl;
+    //cout << *faces << endl;
+    //cout << *(*faces) << endl;
+    //cout << (*faces)[0].p1[0] << endl;//faces[0][0]
+    //cout << (*faces[0]).p1[0] << " - " << (*faces[0]).p1[1] << " - " << (*faces[0]).p1[2] << " - ";
+    //cout << (*faces[1]).p1[0] << " + " << (*faces[1]).p1[1] << " - " << (*faces[1]).p1[2] << endl;
+    cout << "x2" << endl;
+}
+
+ofxSharedMemory<int*> memoryMappedImageId;
+ofxSharedMemory<int*> memoryMappedImageSizeH;
+ofxSharedMemory<int*> memoryMappedImageSizeW;
+ofxSharedMemory<unsigned char*> memoryMappedImagePixels;
+
+bool isConnectedImageId;
+bool isConnectedImageSizeH;
+bool isConnectedImageSizeW;
+bool isConnectedImagePixels;
+
+std::stringstream ImageIdKey;
+std::stringstream ImageSizeHKey;
+std::stringstream ImageSizeWKey;
+std::stringstream ImagePixelsKey;
+
+//bool firstTimeImage = true;
+
+MemoryImageStruct* imagesMemory = new MemoryImageStruct[100];
+int imageIndexMemory = 0;
+
+void DLL_EXPORT ShareImageSetup(int* imageId, int* wPixels, int* hPixels, int* index) {
+
+    *index = imageIndexMemory;
+
+    imageIndexMemory++;
+
+    imagesMemory[*index].ImageIdKey << "ImageId" << *imageId / 10000;
+
+    imagesMemory[*index].ImageSizeHKey << "ImageSizeH" << *imageId / 10000;
+
+    imagesMemory[*index].ImageSizeWKey << "ImageSizeW" << *imageId / 10000;
+
+    imagesMemory[*index].ImagePixelsKey << "ImagePixels" << *imageId / 10000;
+
+    imagesMemory[*index].firstTimeImage = true;
+
+	imagesMemory[*index].memoryMappedImageId.setup(imagesMemory[*index].ImageIdKey.str(), sizeof(int), true);
+    isConnectedImageId = imagesMemory[*index].memoryMappedImageId.connect();
+
+	imagesMemory[*index].memoryMappedImageSizeH.setup(imagesMemory[*index].ImageSizeHKey.str(), sizeof(int), true);
+    isConnectedImageSizeH = imagesMemory[*index].memoryMappedImageSizeH.connect();
+
+	imagesMemory[*index].memoryMappedImageSizeW.setup(imagesMemory[*index].ImageSizeWKey.str(), sizeof(int), true);
+    isConnectedImageSizeW = imagesMemory[*index].memoryMappedImageSizeW.connect();
+
+    imagesMemory[*index].memoryMappedImagePixels.setup(imagesMemory[*index].ImagePixelsKey.str(), (*wPixels) * (*hPixels) * 3, true);
+    isConnectedImagePixels = imagesMemory[*index].memoryMappedImagePixels.connect();
+
+    imagesMemory[*index].memoryMappedImageSizeH.setData(hPixels);
+
+    imagesMemory[*index].memoryMappedImageSizeW.setData(wPixels);
+}
+
+void DLL_EXPORT ReadSharedImageSetup(int* imageId, int* index) {
+
+    imageIndexMemory++;
+
+    *index = imageIndexMemory;
+
+    imagesMemory[*index].ImageIdKey << "ImageId" << *imageId / 10000;
+
+    imagesMemory[*index].ImageSizeHKey << "ImageSizeH" << *imageId / 10000;
+
+    imagesMemory[*index].ImageSizeWKey << "ImageSizeW" << *imageId / 10000;
+
+    imagesMemory[*index].ImagePixelsKey << "ImagePixels" << *imageId / 10000;
+
+    imagesMemory[*index].firstTimeImage = true;
+
+	imagesMemory[*index].memoryMappedImageId.setup(imagesMemory[*index].ImageIdKey.str(), sizeof(int), false);
+    isConnectedImageId = imagesMemory[*index].memoryMappedImageId.connect();
+
+	imagesMemory[*index].memoryMappedImageSizeH.setup(imagesMemory[*index].ImageSizeHKey.str(), sizeof(int), false);
+    isConnectedImageSizeH = imagesMemory[*index].memoryMappedImageSizeH.connect();
+
+	imagesMemory[*index].memoryMappedImageSizeW.setup(imagesMemory[*index].ImageSizeWKey.str(), sizeof(int), false);
+    isConnectedImageSizeW = imagesMemory[*index].memoryMappedImageSizeW.connect();
+
+}
+
+void DLL_EXPORT ShareImage(int* imageId, unsigned char* pixels, int* index) {
+
+    imagesMemory[*index].memoryMappedImageId.setData(imageId);
+
+    imagesMemory[*index].memoryMappedImagePixels.setData(pixels);
+}
+
+void DLL_EXPORT ReadSharedImage(int* Id, int* wPixels, int* hPixels , unsigned char** pixels, int* index) {
+
+    *Id = *(imagesMemory[*index].memoryMappedImageId).getData();
+
+    *hPixels = *(imagesMemory[*index].memoryMappedImageSizeH).getData();
+
+    *wPixels = *(imagesMemory[*index].memoryMappedImageSizeW).getData();
+
+    if (imagesMemory[*index].firstTimeImage){
+    //cout << "Pasodll1 - "<< ImagePixelsKey.str()<< "++++" << endl;
+        imagesMemory[*index].memoryMappedImagePixels.setup(imagesMemory[*index].ImagePixelsKey.str(), (*wPixels) * (*hPixels) * 3, false);
+        isConnectedImagePixels = imagesMemory[*index].memoryMappedImagePixels.connect();
+        imagesMemory[*index].firstTimeImage = false;
+    //cout << "Pasodll2 - " << (isConnectedImagePixels ? "T" : "F") << ImagePixelsKey.str() << endl;
+    }
+
+    *pixels = imagesMemory[*index].memoryMappedImagePixels.getData();
+
+    //cout << "Pasodll5"<< endl;
+}
 
 extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
--- Projects/Dlls/SharedMemory/src/main.h
+++ Projects/Dlls/SharedMemory/src/main.h
@@ -22,20 +22,54 @@ struct FaceStruct
     float p3[3];
 };
 
+struct MemoryImageStruct
+{
+    ofxSharedMemory<int*> memoryMappedImageId;
+    ofxSharedMemory<int*> memoryMappedImageSizeH;
+    ofxSharedMemory<int*> memoryMappedImageSizeW;
+    ofxSharedMemory<unsigned char*> memoryMappedImagePixels;
+    std::stringstream ImageIdKey;
+    std::stringstream ImageSizeHKey;
+    std::stringstream ImageSizeWKey;
+    std::stringstream ImagePixelsKey;
+    bool firstTimeImage;
+};
+
+struct MemoryMeshStruct
+{
+    ofxSharedMemory<int*> memoryMappedMeshId;
+    ofxSharedMemory<int*> memoryMappedMeshSize;
+    ofxSharedMemory<FaceStruct*> memoryMappedMesh;
+    std::stringstream MeshIdKey;
+    std::stringstream MeshSizeKey;
+    std::stringstream MeshKey;
+    bool firstTimeMesh;
+    int maxNumberFaces;
+
+};
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-void DLL_EXPORT ReadSharedImage(int* Id, int* wPixels, int* hPixels, unsigned char** Pixels);
+//MESH
+void DLL_EXPORT ShareMeshSetup(int* meshId, int* index);
 
-void DLL_EXPORT MemoryCheck(int* Id);
+void DLL_EXPORT ReadSharedMeshSetup(int* meshId, int* index);
 
-void DLL_EXPORT ShareImage(int* imageId, unsigned char* pixels, int* wPixels, int* hPixels);
+void DLL_EXPORT ShareMesh( int* meshId, int* numberFaces, FaceStruct* faces, int* index);
 
-void DLL_EXPORT ShareMesh(int idMesh, int numberFaces, FaceStruct* faces);
+void DLL_EXPORT ReadSharedMesh(int* meshId, int* numberFaces, FaceStruct** faces, int* index);
 
-void DLL_EXPORT ReadSharedMesh(int* Id, int* numberFaces, FaceStruct** faces);
+//IMAGE
+void DLL_EXPORT ShareImageSetup(int* imageId, int* wPixels, int* hPixels, int* index);
+
+void DLL_EXPORT ReadSharedImageSetup(int* imageId, int* index);
+
+void DLL_EXPORT ShareImage(int* imageId, unsigned char* pixels, int* index);
+
+void DLL_EXPORT ReadSharedImage(int* Id, int* wPixels, int* hPixels , unsigned char** pixels, int* index) ;
 
 #ifdef __cplusplus
 }
